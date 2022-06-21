6521d3df1663f5bf7de5da53074e522044d63430
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-21 11:15:13 +0200

    initialize of featurization

---
+++ src/stage_02_featurization.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+import random
+from src.utils import read_yaml,create_directories
+from src.utils.data_mgmt import process_posts
+
+STAGE="Prepare_Data"
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+def main(config_path, params_path):
+    ## read config file  
+    config=read_yaml(config_path)
+    params=read_yaml(params_path)
+
+
+if __name__== '__main__':
+    args=argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="configs/config.yaml")
+    args.add_argument("--params","-p",default="params.yaml")
+    parsed_args=args.parse_args()
+
+    try:
+        logging.info("\n*********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stahe {STAGE} completed <<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
