8db5daf9afac63813b13d4b9efde4dc5ed776e2a
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-21 12:12:27 +0200

    stage_02 done

--- src/stage_02_featurization.py
+++ src/stage_02_featurization.py
@@ -4,9 +4,10 @@ import shutil
 from tqdm import tqdm
 import logging
 import random
-from src.utils import read_yaml,create_directories
+from src.utils import read_yaml,create_directories,get_df
 from src.utils.data_mgmt import process_posts
-
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 STAGE="Prepare_Data"
 
 logging.basicConfig(
@@ -19,6 +20,48 @@ def main(config_path, params_path):
     ## read config file  
     config=read_yaml(config_path)
     params=read_yaml(params_path)
+
+    artifacts = config["artifacts"]
+    prepare_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
+
+    train_data_path = os.path.join(prepare_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepare_data_dir_path,artifacts["TEST_DATA"])
+
+    feturize_data_dir_path=os.path.join(artifacts[" ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
+    create_directories([feturize_data_dir_path])
+    featurized_train_data_path=os.path.join(feturize_data_dir_path, artifacts["FEATURIZED_DATA_TRAIN"])
+    featurized_test_data_path=os.path.join(feturize_data_dir_path, artifacts["FEATURIZE_DATA_TEST"])
+
+
+    max_features=params["featurize"]["max_features"]
+    n_grams=params["featurize"]["n_grams"]
+    #For training
+
+    df_train=get_df(train_data_path)
+    train_words=np.array(df_train.text.str.lower().values.astype("U"))
+
+    bag_of_words= CountVectorizer(
+        stop_words="english",
+        max_features=max_features,
+        ngram_range=(1,n_grams)
+    )
+    bag_of_words.fit(train_words)
+    train_words_binary_matrix=bag_of_words.transform(train_words)
+    tfidf=TfidfTransformer(smooth_idf=False)
+    tfidf.fit(train_words_binary_matrix)
+    train_words_tfidf_matrix=tfidf.transform(train_words_binary_matrix)
+
+    # for test data
+    df_test=get_df(test_data_path)
+    test_words=np.array(df_test.text.str.lower().values.astype("U"))
+
+   
+    
+    test_words_binary_matrix=bag_of_words.transform(test_words)
+    
+    test_words_tfidf_matrix=tfidf.transform(test_words_binary_matrix)
+
+
 
 
 if __name__== '__main__':
