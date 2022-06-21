3e5f121289300c66990cc1f1c96f0c3e53c6764f
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-21 10:04:29 +0200

    artifacts directory created

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -9,7 +9,7 @@ from src.utils import read_yaml,create_directories
 STAGE="Prepare_Data"
 
 logging.basicConfig(
-    filename=os.path.join("logs", 'running_log'),
+    filename=os.path.join("logs", 'running_logs'),
     level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a"
@@ -20,6 +20,17 @@ def main(config_path, params_path):
     ## read config file  
     config=read_yaml(config_path)
     params=read_yaml(params_path)
+    source_data_dir=config["source_data"]["data_dir"]
+    source_data_file=config["source_data"]["data_file"]
+    source_data_path=os.path.join(source_data_dir, source_data_file)
+
+    split=params["prepare"]["split"]# split ratio
+    seed=params["prepare"]["seed"]
+    random.seed(seed)
+    artifacts=config["artifacts"]
+    prepare_data_dir_path=os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
+    create_directories([prepare_data_dir_path])
+
 
 
 
@@ -28,7 +39,7 @@ def main(config_path, params_path):
 if __name__== '__main__':
     args=argparse.ArgumentParser()
     args.add_argument("--config","-c",default="configs/config.yaml")
-    args.add_arguments("--params","-p",default="params.yaml")
+    args.add_argument("--params","-p",default="params.yaml")
     parsed_args=args.parse_args()
 
     try:
