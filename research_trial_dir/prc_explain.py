0177f1f2e2353164a3739f7a737cef652dec6e9b
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-23 12:34:01 +0200

    prec explain

---
+++ research_trial_dir/prc_explain.py
@@ -0,0 +1,2 @@
+precision,recall, prc_threshold=[0.1,0.2,0.4,0.9,0.99,0.22,0.66],[0.1,0.2,0.4,0.9,0.99,0.22,0.66],[0.1,0.2,0.4,0.9,0.99,0.22,0.66]
+print(list(zip(precision,recall, prc_threshold))[::3])
