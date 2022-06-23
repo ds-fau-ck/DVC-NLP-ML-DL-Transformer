997c7c7317353241f47e8df69aa3bb5511a8cda8
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-23 11:24:35 +0200

    stage_03 completed

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -7,6 +7,8 @@ import random
 from src.utils import read_yaml,create_directories,get_df
 from src.utils.data_mgmt import process_posts
 import numpy as np
+import joblib
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer 
 
 STAGE="TRAIN DATA"
@@ -21,6 +23,37 @@ def main(config_path, params_path):
     ## read config file  
     config=read_yaml(config_path)
     params=read_yaml(params_path)
+
+    artifacts = config["artifacts"]
+    feturize_data_dir_path=os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
+    featurized_train_data_path=os.path.join(feturize_data_dir_path, artifacts["FEATURIZED_DATA_TRAIN"])
+    
+    
+    model_dir=artifacts["MODEL_DIR"]
+    model_dir_path=os.path.join(artifacts["ARTIFACTS_DIR"], model_dir)
+    create_directories([model_dir_path])
+    model_name=artifacts["MODEL_NAME"]
+    model_path=os.path.join(model_dir_path, model_name)
+    matrix=joblib.load(featurized_train_data_path)
+    labels=np.squeeze(matrix[:,1].toarray())
+    X=matrix[:,2:]
+
+    logging.info(f"input matrix size: {matrix.shape}")
+    logging.info(f"X matrix size: {X.shape}")
+    logging.info(f"Y matrix size  or labels: {labels.shape}")
+    seed=params["train"]["seed"]
+    n_est=params["train"]["n_est"]
+    n_jobs=params["train"]["n_jobs"]
+    min_split=params["train"]["min_split"]
+    model=RandomForestClassifier(
+        n_estimators=n_est,
+        min_samples_split=min_split,
+        n_jobs=n_jobs, random_state=seed
+    )
+    model.fit(X, labels)
+    joblib.dump(model, model_path)
+    logging.info(f"save our model at: : {model_path}")
+
 
 
 if __name__== '__main__':
